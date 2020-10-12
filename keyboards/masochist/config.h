7abd813b72655ebaf21fc15cfa180be30b79124d
Reid Schneyer <reschneyer89@gmail.com>
2020-10-12 12:09:54 -0500

    updated README and created config.h

---
+++ keyboards/masochist/config.h
@@ -0,0 +1,40 @@
+/*
+Copyright 2020 Reid Schneyer
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+#define VENDOR_ID    0xFEED
+#define PRODUCT_ID   0x6464
+#define DEVICE_VER   0x0001
+#define MANUFACTURER Reid Schneyer
+#define PRODUCT      Masochist
+#define DESCRIPTION  Keyboard firmware for Masochist
+
+#define MATRIX_ROWS  4
+#define MATRIX_COLS  10
+
+#define MATRIX_COL_PINS { C5, C4, C3, C2, C1, C0, D0, D1, D4, B0 }
+#define MATRIX_ROW_PINS {B4, B3, B1, B2}
+#define DIODE_DIRECTION COL2ROW
+#define UNUSED_PINS
+
+#define DEBOUNCE 5
+
+#define LOCKING_SUPPORT_ENABLE
+#define LOCKING_RESYNC_ENABLE
---
+++ keyboards/masochist/readme.md
@@ -0,0 +1,14 @@
+# Masochist
+![masochist](https://raw.githubusercontent.com/RSchneyer/masochist/master/media/masochist_front.png)
+A through-hole component only 27% keyboard
+
+Keyboard Maintainer: [Reid Schneyer](https://github.com/RSchneyer)
+
+Hardware availability: [Gerber Files](https://github.com/RSchneyer/masochist/tree/master/gerbers)
+
+Make example for this keyboard (after setting up your build environment):
+    make masochist:default
+
+See [build environment setup](https://docs.qmk.fm/install-build-tools) then the [make instructions](https://docs.qmk.fm/faq/build-compile-qmk) for more information.
+
+Please see [RSchneyer/masochist](https://www.github.com/RSchneyer/masochist) for **build instructions**, parts lists, and Gerber files.
